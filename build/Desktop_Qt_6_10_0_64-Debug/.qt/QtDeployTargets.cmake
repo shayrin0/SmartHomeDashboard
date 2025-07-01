ca9c7e8b2a2ca5732a8c7f33438a151ce8221e7b
Shirin <shayrin.panahi@gmail.com>
2025-07-01 17:22:27 -0400

    Ignore build directory

--- build/Desktop_Qt_6_10_0_64-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appSmartHomeDashboard_FILE /home/shayrin/GitHub/SmartHomeDashboard/build/Desktop_Qt_6_10_0_64-Debug/appSmartHomeDashboard)
-set(__QT_DEPLOY_TARGET_appSmartHomeDashboard_TYPE EXECUTABLE)
--- build/Desktop_Qt_6_10_0_64-Debug/.qt/appSmartHomeDashboard_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appSmartHomeDashboard")
-set(working_dir "/home/shayrin/GitHub/SmartHomeDashboard")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/shayrin/GitHub/SmartHomeDashboard/build/Desktop_Qt_6_10_0_64-Debug/.qt/appSmartHomeDashboard_res.txt")
