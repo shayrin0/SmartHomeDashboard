7721d79e51c69e5dc78cdf84add5872e65dfe558
Shirin <shayrin.panahi@gmail.com>
2025-07-01 18:03:08 -0400

    Register the DeviceListViewModel

--- main.cpp
+++ main.cpp
@@ -1,18 +1,31 @@
+#include "ViewModels/DeviceListViewModel.h"
+
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
 
+/// @brief Main function for the Smart Home Dashboard application.
+/// Initializes the application, sets up the QML engine, and loads the main QML file
+/// @param argc
+/// @param argv 
+/// @return The exit code of the application
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
+   QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreationFailed,
-        &app,
-        []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
-    engine.loadFromModule("SmartHomeDashboard", "Main");
+   QQmlApplicationEngine engine;
 
-    return app.exec();
+   DeviceListViewModel deviceViewModel;
+   engine.rootContext()->setContextProperty("deviceViewModel", &deviceViewModel);
+
+   QObject::connect(
+      &engine,
+      &QQmlApplicationEngine::objectCreationFailed,
+      &app,
+      []()
+      { QCoreApplication::exit(-1); },
+      Qt::QueuedConnection);
+   engine.loadFromModule("SmartHomeDashboard", "Main");
+
+   return app.exec();
 }
